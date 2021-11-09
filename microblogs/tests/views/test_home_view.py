a2116ccc25f70aaf7e81affc8434255bfc153c67
JohanLi_2333 <zhonghan.li@kcl.ac.uk>
2021-11-09 11:47:57 +0000

    make home @login_prohibited

---
+++ microblogs/tests/views/test_home_view.py
@@ -0,0 +1,37 @@
+"""Tests of the home view."""
+from django.test import TestCase
+from django.urls import reverse
+from microblogs.models import User
+
+class HomeViewTestCase(TestCase):
+    """Tests of the home view."""
+
+    fixtures=['microblogs/tests/fixtures/default_user.json']
+
+    def setUp(self):
+        self.url = reverse('home')
+        self.form_input = {
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'username': '@janedoe',
+            'email': 'janedoe@example.org',
+            'bio': 'My bio',
+            'new_password': 'Password123',
+            'password_confirmation': 'Password123'
+        }
+        self.user = User.objects.get(username='@johndoe')
+
+    def test_home_url(self):
+        self.assertEqual(self.url,'/')
+
+    def test_get_home(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_get_home_with_redirects_when_logged_in(self):
+        self.client.login(username=self.user.username, password="Password123")
+        response = self.client.get(self.url, follow=True)
+        redirect_url = reverse('feed')
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
--- microblogs/views.py
+++ microblogs/views.py
@@ -44,6 +44,7 @@ def log_out(request):
     logout(request)
     return redirect('home')
 
+@login_prohibited
 def home(request):
     return render(request, 'home.html')
 
